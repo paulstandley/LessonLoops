b4b6f87afeee02c01a83cacfd054b345152b152a
Paul Standley <paulstandley1972@gmail.com>
2019-04-24 18:04:38 +0100

    Pass by ref

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace LessonLoops
 {
@@ -10,6 +11,19 @@ namespace LessonLoops
     {
         static void Main(string[] args)
         {
+            // pass by ref default pass by value
+            int num1 = 10;
+            int num2 = 20;
+            Console.WriteLine("before the swop function is called num1 : {0} num2 : {1}", num1, num2);
+            Swop(ref num1, ref num2);
+            Console.WriteLine("after the swop function is called num1 : {0} num2 : {1}", num1, num2);
+            Console.ReadLine();
+        }
+        public static void Swop(ref int num1, ref int num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
         }
     }
 }
