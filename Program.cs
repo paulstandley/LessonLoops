3fd950a307a690c22bce73e823b17f878a3fc3aa
Paul Standley <paulstandley1972@gmail.com>
2019-04-25 20:36:21 +0100

    struct

--- Program.cs
+++ Program.cs
@@ -11,22 +11,32 @@ namespace LessonLoops
     {
         static void Main(string[] args)
         {
-            // enum a custum data type for key value paires
-            CarColor car1 = CarColor.bule;
-            PaintCar(car1);
+            Rectangle rect1;
+            rect1.width = 200;
+            rect1.length = 50;
+            Console.WriteLine("the area of the rect1 is {0}", rect1.Area());
+            Rectangle rect2 = new Rectangle(20, 40);
+            Console.WriteLine("the area of the rect2 is {0}", rect2.Area());
+            // values passed by value not ref
+            rect2 = rect1;
+            rect1.length = 30;
+            Console.WriteLine("the area of rect1 is {0}, rect2 is {1} and rect1.Length {2}", rect1.Area(), rect2.Area(), rect1.length);
             Console.ReadLine();
         }
-        enum CarColor : byte
+        struct Rectangle
         {
-            orange = 1,
-            green,
-            bule,
-            red,
-            black
-        }
-        static void PaintCar(CarColor cc)
-        {
-            Console.WriteLine("The car is painted {0} and the index is {1}", cc, (int)cc);
+            public double width;
+            public double length;
+            // mehod constructor
+            public Rectangle(double w = 1, double l = 1)
+            {
+                width = 1;
+                length = 1;
+            }
+            public double Area()
+            {
+                return width * length;
+            }
         }
     }
 }
