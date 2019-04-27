7429a89bae80abc213efa1b2c8090cce0ab18204
Paul Standley <paulstandley1972@gmail.com>
2019-04-27 14:13:59 +0100

    over load class constuctor

---
+++ Animalcs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LessonLoops
+{
+    class Animalcs
+    {
+        public string name;
+        public string sound;
+
+        public Animalcs()
+        {
+            name = "No Name";
+            sound = "No Sound";
+        }
+
+        // overload constuctor
+        public Animalcs(string name = "No Name", string sound = "No Sound")
+        {
+            this.name = name;
+            this.sound = sound;
+        }
+    }
+}
